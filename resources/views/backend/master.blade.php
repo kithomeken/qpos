0465246fd2fd72ef98249332d0123fb110097797
Kennedy Kithome <kithomeken@gmail.com>
2026-04-27 09:22:01 +0300

    Title Removal & Margin Top Addition For a Cleaner UI/UX

--- resources/views/backend/master.blade.php
+++ resources/views/backend/master.blade.php
@@ -142,23 +142,9 @@
 
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0">
-                                @yield('title')
-                            </h1>
-                        </div><!-- /.col -->
-                    </div><!-- /.row -->
-                </div><!-- /.container-fluid -->
-            </div>
-            <!-- /.content-header -->
-
 
             <!-- Main content -->
-            <section class="content">
+            <section class="content h-100" style="padding-top: 60px">
                 <!-- container-fluid -->
                 <div class="container-fluid">
 
